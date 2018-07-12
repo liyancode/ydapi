90f6d34637974e67039a76c8248b63a38a269618
liyancode <liyan.code@gmail.com>
2018-07-12 12:00:03 +0800

    rm unused method process_request

--- _helpers/helper.rb
+++ _helpers/helper.rb
@@ -42,23 +42,6 @@ module YDAPI
           nil
         end
       end
-
-      def Helper.process_request(req, scope)
-        begin
-          scopes, user = req.env.values_at :scopes, :user
-          username = user['username']
-
-          if scopes.include?(scope)
-            yield req, username
-          else
-            @@logger.info("#{self}.process_request #{req.env["REQUEST_METHOD"]} #{req.fullpath} username=#{username} 403 Forbidden.")
-            halt 403
-          end
-        rescue Exception => e
-          @@logger.error("#{self}.process_request #{req.env["REQUEST_METHOD"]} #{req.fullpath} username=#{username} 500 Internal Server Error. Exception:#{e}")
-          halt 500
-        end
-      end
     end
   end
 end
