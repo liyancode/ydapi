df9ac235e05a1a1c946e2bfd9e79f41ef8d76fc5
liyancode <liyan.code@gmail.com>
2018-07-15 12:22:54 +0800

    RawMaterial

---
+++ _biz/entity/raw_material.rb
@@ -0,0 +1,18 @@
+module YDAPI
+  module BizEntity
+    class RawMaterial
+      attr_accessor :id
+      attr_accessor :raw_material_id
+      attr_accessor :added_by_user_name
+      attr_accessor :name
+      attr_accessor :raw_material_type_id
+      attr_accessor :measurement_unit
+      attr_accessor :specification
+      attr_accessor :features
+      attr_accessor :use_for
+      attr_accessor :description
+      attr_accessor :comment
+      attr_accessor :status
+    end
+  end
+end
