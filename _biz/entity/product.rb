b75eef4355544e5d0dbb0561db1f460c85bc7a3e
liyancode <liyan.code@gmail.com>
2018-07-15 13:06:23 +0800

    product and type

---
+++ _biz/entity/product.rb
@@ -0,0 +1,19 @@
+module YDAPI
+  module BizEntity
+    class Product
+      attr_accessor :id
+      attr_accessor :product_id
+      attr_accessor :added_by_user_name
+      attr_accessor :name
+      attr_accessor :product_type_id
+      attr_accessor :measurement_unit
+      attr_accessor :specification
+      attr_accessor :raw_material_ids
+      attr_accessor :features
+      attr_accessor :use_for
+      attr_accessor :description
+      attr_accessor :comment
+      attr_accessor :status
+    end
+  end
+end
---
+++ _biz/entity/product_type.rb
@@ -0,0 +1,13 @@
+module YDAPI
+  module BizEntity
+    class ProductType
+      attr_accessor :id
+      attr_accessor :product_type_id
+      attr_accessor :added_by_user_name
+      attr_accessor :name
+      attr_accessor :description
+      attr_accessor :comment
+      attr_accessor :status
+    end
+  end
+end
