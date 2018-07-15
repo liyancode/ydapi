5aaa4ca0cb982beeaf03e743a5724ec553b4669e
liyancode <liyan.code@gmail.com>
2018-07-15 12:23:47 +0800

    RawMaterialType

---
+++ _biz/entity/raw_material_type.rb
@@ -0,0 +1,13 @@
+module YDAPI
+  module BizEntity
+    class RawMaterialType
+      attr_accessor :id
+      attr_accessor :raw_material_type_id
+      attr_accessor :added_by_user_name
+      attr_accessor :name
+      attr_accessor :description
+      attr_accessor :comment
+      attr_accessor :status
+    end
+  end
+end
