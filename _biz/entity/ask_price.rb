91119cda9064ca7ddb5df0322560c938fbf35cbb
liyancode <liyan.code@gmail.com>
2018-07-21 22:28:36 +0800

    ask_price entity

---
+++ _biz/entity/ask_price.rb
@@ -0,0 +1,16 @@
+module YDAPI
+  module BizEntity
+    class AskPrice
+      attr_accessor :id
+      attr_accessor :ask_price_id
+      attr_accessor :added_by_user_name
+      attr_accessor :customer_id
+      attr_accessor :product_ids
+      attr_accessor :description
+      attr_accessor :approve_status
+      attr_accessor :approve_by_user_name
+      attr_accessor :comment
+      attr_accessor :status
+    end
+  end
+end
