9c6ffe44ab833e9ffbe9818cd8a47849233e09df
liyancode <liyan.code@gmail.com>
2018-07-23 22:46:12 +0800

    add contract model funcs

--- _biz/model/orders_model.rb
+++ _biz/model/orders_model.rb
@@ -7,6 +7,10 @@ module YDAPI
       @@ask_price=YDAPI::BizEntity::AskPrice
       @@ask_prices=YDAPI::BizModel::DBModel::AskPrices
 
+      @@contract=YDAPI::BizEntity::Contract
+      @@contracts=YDAPI::BizModel::DBModel::Contracts
+
+      # ---- ask_prices ----↓
       def OrdersModel.add_new_ask_price(ask_price)
         if ask_price.is_a?(@@ask_price)
           new_id = @@helper.new_step1_id(@@ask_prices.func_get_max_ask_price_id)
@@ -48,6 +52,67 @@ module YDAPI
             ask_prices_array<<row.values
           }
           {:ask_prices => ask_prices_array}
+        else
+          nil
+        end
+      end
+
+      # ---- contracts ----↓
+      def OrdersModel.add_new_contract(contract)
+        if contract.is_a?(@@contract)
+          new_id = @@helper.new_step1_id(@@contracts.func_get_max_contract_id)
+          if new_id != nil
+            contract.contract_id = new_id
+            @@logger.info("#{self}.add_new_contract, contract=#{contract}")
+            @@contracts.func_add(contract)
+          else
+            @@logger.error("#{self}.add_new_contract, new_product_id error")
+            nil
+          end
+        else
+          @@logger.error("#{self}.add_new_contract, contract is not a #{@@contract}")
+          nil
+        end
+      end
+
+      def OrdersModel.delete_contract_by_contract_id(contract_id)
+        @@logger.info("#{self}.delete_contract_by_contract_id(#{contract_id})")
+        @@contracts.func_delete(contract_id)
+      end
+
+      def OrdersModel.update_contract(contract)
+        @@logger.info("#{self}.update_contract, contract=#{contract}")
+        @@contracts.func_update(contract)
+      end
+
+      def OrdersModel.get_contract_by_contract_id(contract_id)
+        @@logger.info("#{self}.get_contract_by_contract_id(#{contract_id})")
+        @@contracts.func_get(contract_id)
+      end
+
+      def OrdersModel.get_contracts_by_user_name(user_name)
+        @@logger.info("#{self}.get_contracts_by_user_name(#{user_name})")
+        contracts = @@contracts.func_get_all_by_user_name(user_name)
+        if contracts
+          contracts_array = []
+          contracts.each{|row|
+            contracts_array<<row.values
+          }
+          {:contracts => contracts_array}
+        else
+          nil
+        end
+      end
+
+      def OrdersModel.get_contracts_by_sign_user_name(sign_by_user_name)
+        @@logger.info("#{self}.get_contracts_by_sign_user_name(#{sign_by_user_name})")
+        contracts = @@contracts.func_get_all_by_sign_user(sign_by_user_name)
+        if contracts
+          contracts_array = []
+          contracts.each{|row|
+            contracts_array<<row.values
+          }
+          {:contracts => contracts_array}
         else
           nil
         end
