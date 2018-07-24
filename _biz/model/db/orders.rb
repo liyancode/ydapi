a67a7aff7c6e9e38d93ed825fc05891af60354b2
liyancode <liyan.code@gmail.com>
2018-07-24 21:57:56 +0800

    order dbo

---
+++ _biz/model/db/orders.rb
@@ -0,0 +1,107 @@
+module YDAPI
+  module BizModel
+    module DBModel
+      class Orders < Sequel::Model(:orders)
+        @@logger=BIZ_MODEL_LOGGER
+        def Orders.func_add(order)
+          begin
+            Orders.create do |c|
+              c.order_id=order.order_id
+              c.added_by_user_name=order.added_by_user_name
+              c.contract_id=order.contract_id
+              c.sign_by_user_name=order.sign_by_user_name
+              c.customer_id=order.customer_id
+              c.start_date=order.start_date
+              c.end_date=order.end_date
+              c.total_value=order.total_value
+              c.pay_type=order.pay_type
+              c.paid_value=order.paid_value
+              c.order_status=order.order_status
+              c.order_status_update_by=order.order_status_update_by
+              c.description=order.description
+              c.comment=order.comment
+            end
+          rescue Exception => e
+            @@logger.error("#{self}.func_add(#{order}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def Orders.func_delete(order_id)
+          begin
+            Orders[order_id: order_id].update(status: 0)
+            true
+          rescue Exception => e
+            @@logger.error("#{self}.func_delete(#{order_id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def Orders.func_update(order)
+          begin
+            exist_c=Orders[order_id: order.order_id]
+            new_c=exist_c.update(
+                contract_id:order.contract_id,
+                sign_by_user_name:order.sign_by_user_name,
+                customer_id:order.customer_id,
+                start_date:order.start_date,
+                end_date:order.end_date,
+                total_value:order.total_value,
+                pay_type:order.pay_type,
+                paid_value:order.paid_value,
+                order_status:order.order_status,
+                order_status_update_by:order.order_status_update_by,
+                is_finished:order.is_finished,
+                description:order.description,
+                comment:order.comment
+            )
+            if new_c
+              new_c
+            else
+              exist_c
+            end
+          rescue Exception => e
+            @@logger.error("#{self}.func_update(#{order}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def Orders.func_get(order_id)
+          begin
+            Orders[order_id: order_id]
+          rescue Exception => e
+            @@logger.error("#{self}.func_get(#{order_id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def Orders.func_get_all_by_user_name(user_name)
+          begin
+            Orders.dataset.where(added_by_user_name:user_name).where(status:1).all
+          rescue Exception => e
+            @@logger.error("#{self}.func_get_all_by_user_name(user_name) Exception:#{e}")
+            nil
+          end
+        end
+
+        def Orders.func_get_all_by_sign_user(sign_by_user_name)
+          begin
+            Orders.dataset.where(sign_by_user_name:sign_by_user_name).where(status:1).all
+          rescue Exception => e
+            @@logger.error("#{self}.func_get_all_by_user_name(user_name) Exception:#{e}")
+            nil
+          end
+        end
+
+        def Orders.func_get_max_order_id
+          begin
+            Orders.last.order_id
+          rescue Exception => e
+            @@logger.error("#{self}.func_get_max_order_id Exception:#{e}")
+            nil
+          end
+        end
+      end
+    end
+  end
+end
