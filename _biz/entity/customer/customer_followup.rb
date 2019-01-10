5845fc70bec716935e04cb8b633c24c65d1cc67f
liyancode <liyan.code@gmail.com>
2019-01-10 23:42:17 +0800

    customer*

---
+++ _biz/entity/customer/customer_followup.rb
@@ -0,0 +1,18 @@
+module YDAPI
+  module BizEntity
+    class CustomerFollowup
+      attr_accessor :id
+      attr_accessor :created_by
+      attr_accessor :last_update_by
+      attr_accessor :status
+      attr_accessor :comment
+      attr_accessor :customer_id
+      attr_accessor :user_name
+      attr_accessor :followup_description
+      attr_accessor :followup_status
+      attr_accessor :followup_method
+      attr_accessor :followup_method_my_id
+      attr_accessor :followup_method_customer_id
+    end
+  end
+end
