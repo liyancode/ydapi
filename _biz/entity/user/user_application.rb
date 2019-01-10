2055918c67c9f290f69c725955d4944063d74169
liyancode <liyan.code@gmail.com>
2019-01-10 23:44:25 +0800

    user*

---
+++ _biz/entity/user/user_application.rb
@@ -0,0 +1,17 @@
+module YDAPI
+  module BizEntity
+    class UserApplication
+      attr_accessor :id
+      attr_accessor :created_by
+      attr_accessor :last_update_by
+      attr_accessor :status
+      attr_accessor :comment
+      attr_accessor :application_id
+      attr_accessor :user_name
+      attr_accessor :type
+      attr_accessor :description
+      attr_accessor :approve_by
+      attr_accessor :approve_status
+    end
+  end
+end
