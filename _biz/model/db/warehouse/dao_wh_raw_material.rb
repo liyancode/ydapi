54d8a324a1515a41b425685f2abbf071e5a1b306
liyancode <liyan.code@gmail.com>
2019-01-10 23:45:30 +0800

    warehouse*

---
+++ _biz/model/db/warehouse/dao_wh_raw_material.rb
@@ -0,0 +1,104 @@
+module YDAPI
+  module BizModel
+    module DBModel
+      class DAO_WHRawMaterial < Sequel::Model(:wh_raw_material)
+        @@logger=BIZ_MODEL_LOGGER
+        def DAO_WHRawMaterial.func_add(wh_raw_material)
+          begin
+            DAO_WHRawMaterial.create do |obj|
+              obj.created_by = wh_raw_material.created_by
+              obj.last_update_by = wh_raw_material.last_update_by
+              obj.comment = wh_raw_material.comment
+
+              obj.wh_id = wh_raw_material.wh_id
+              obj.wh_id_sub = wh_raw_material.wh_id_sub
+              obj.wh_location = wh_raw_material.wh_location
+              obj.wh_inner_location = wh_raw_material.wh_inner_location
+              obj.principal = wh_raw_material.principal
+              obj.name = wh_raw_material.name
+              obj.specification = wh_raw_material.specification
+              obj.description = wh_raw_material.description
+              obj.count = wh_raw_material.count
+              obj.count_unit = wh_raw_material.count_unit
+              obj.unit_price = wh_raw_material.unit_price
+              obj.weight = wh_raw_material.weight
+              obj.weight_unit = wh_raw_material.weight_unit
+              obj.other = wh_raw_material.other
+            end
+          rescue Exception => e
+            @@logger.error("#{self}.func_add(#{wh_raw_material.wh_id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterial.func_delete(id)
+          begin
+            DAO_WHRawMaterial[id: id].update(status: 0)
+            true
+          rescue Exception => e
+            @@logger.error("#{self}.func_delete(#{id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterial.func_update(wh_raw_material)
+          begin
+            exist_item=DAO_WHRawMaterial[id: wh_raw_material.id]
+            new_item=exist_item.update(
+                # password: wh_raw_material.password,
+                last_update_by: wh_raw_material.last_update_by,
+                comment: wh_raw_material.comment,
+                wh_location: wh_raw_material.wh_location,
+                wh_inner_location: wh_raw_material.wh_inner_location,
+                principal: wh_raw_material.principal,
+                name: wh_raw_material.name,
+                specification: wh_raw_material.specification,
+                description: wh_raw_material.description,
+                count: wh_raw_material.count,
+                count_unit: wh_raw_material.count_unit,
+                unit_price: wh_raw_material.unit_price,
+                weight: wh_raw_material.weight,
+                weight_unit: wh_raw_material.weight_unit,
+                other: wh_raw_material.other
+            )
+            if new_item
+              new_item
+            else
+              exist_item
+            end
+          rescue Exception => e
+            @@logger.error("#{self}.func_update(#{wh_raw_material.id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterial.func_get(id)
+          begin
+            DAO_WHRawMaterial[id: id]
+          rescue Exception => e
+            @@logger.error("#{self}.func_get(#{id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterial.func_get_one_rm(wh_id)
+          begin
+            DAO_WHRawMaterial.dataset.where(wh_id:wh_id).where(status:1).all
+          rescue Exception => e
+            @@logger.error("#{self}.func_get_one_rm(#{wh_id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterial.func_get_all
+          begin
+            DAO_WHRawMaterial.dataset.where(status:1).all
+          rescue Exception => e
+            @@logger.error("#{self}.func_get_all Exception:#{e}")
+            nil
+          end
+        end
+      end
+    end
+  end
+end
---
+++ _biz/model/db/warehouse/dao_wh_raw_material_history.rb
@@ -0,0 +1,126 @@
+module YDAPI
+  module BizModel
+    module DBModel
+      class DAO_WHRawMaterialHistory < Sequel::Model(:wh_raw_material_history)
+        @@logger=BIZ_MODEL_LOGGER
+        def DAO_WHRawMaterialHistory.func_add(wh_raw_material_history)
+          begin
+            DAO_WHRawMaterialHistory.create do |obj|
+              obj.created_by = wh_raw_material_history.created_by
+              obj.last_update_by = wh_raw_material_history.last_update_by
+              obj.comment = wh_raw_material_history.comment
+
+              obj.wh_id = wh_raw_material_history.wh_id
+              obj.wh_id_sub = wh_raw_material_history.wh_id_sub
+              obj.record_type = wh_raw_material_history.record_type
+              obj.update_what = wh_raw_material_history.update_what
+              obj.order_contract_id = wh_raw_material_history.order_contract_id
+              obj.inbound_count = wh_raw_material_history.inbound_count
+              obj.inbound_weight = wh_raw_material_history.inbound_weight
+              obj.inbound_unit_price = wh_raw_material_history.inbound_unit_price
+              obj.inbound_total_price = wh_raw_material_history.inbound_total_price
+              obj.inbound_from = wh_raw_material_history.inbound_from
+              obj.inbound_principal = wh_raw_material_history.inbound_principal
+              obj.inbound_at = wh_raw_material_history.inbound_at
+              obj.outbound_count = wh_raw_material_history.outbound_count
+              obj.outbound_weight = wh_raw_material_history.outbound_weight
+              obj.outbound_unit_price = wh_raw_material_history.outbound_unit_price
+              obj.outbound_total_price = wh_raw_material_history.outbound_total_price
+              obj.outbound_to = wh_raw_material_history.outbound_to
+              obj.outbound_principal = wh_raw_material_history.outbound_principal
+              obj.outbound_at = wh_raw_material_history.outbound_at
+              obj.other = wh_raw_material_history.other
+            end
+          rescue Exception => e
+            @@logger.error("#{self}.func_add(#{wh_raw_material_history.wh_id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterialHistory.func_delete(id)
+          begin
+            DAO_WHRawMaterialHistory[id: id].update(status: 0)
+            true
+          rescue Exception => e
+            @@logger.error("#{self}.func_delete(#{id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterialHistory.func_update(wh_raw_material_history)
+          begin
+            exist_item=DAO_WHRawMaterialHistory[id: wh_raw_material_history.id]
+            new_item=exist_item.update(
+                last_update_by: wh_raw_material_history.last_update_by,
+                comment: wh_raw_material_history.comment,
+                wh_id: wh_raw_material_history.wh_id,
+                wh_id_sub: wh_raw_material_history.wh_id_sub,
+                record_type: wh_raw_material_history.record_type,
+                update_what: wh_raw_material_history.update_what,
+                order_contract_id: wh_raw_material_history.order_contract_id,
+                inbound_count: wh_raw_material_history.inbound_count,
+                inbound_weight: wh_raw_material_history.inbound_weight,
+                inbound_unit_price: wh_raw_material_history.inbound_unit_price,
+                inbound_total_price: wh_raw_material_history.inbound_total_price,
+                inbound_from: wh_raw_material_history.inbound_from,
+                inbound_principal: wh_raw_material_history.inbound_principal,
+                inbound_at: wh_raw_material_history.inbound_at,
+                outbound_count: wh_raw_material_history.outbound_count,
+                outbound_weight: wh_raw_material_history.outbound_weight,
+                outbound_unit_price: wh_raw_material_history.outbound_unit_price,
+                outbound_total_price: wh_raw_material_history.outbound_total_price,
+                outbound_to: wh_raw_material_history.outbound_to,
+                outbound_principal: wh_raw_material_history.outbound_principal,
+                outbound_at: wh_raw_material_history.outbound_at,
+                other: wh_raw_material_history.other
+            )
+            if new_item
+              new_item
+            else
+              exist_item
+            end
+          rescue Exception => e
+            @@logger.error("#{self}.func_update(#{wh_raw_material_history.id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterialHistory.func_get(id)
+          begin
+            DAO_WHRawMaterialHistory[id: id]
+          rescue Exception => e
+            @@logger.error("#{self}.func_get(#{id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterialHistory.func_get_all_by_wh_id(wh_id)
+          begin
+            DAO_WHRawMaterialHistory.dataset.where(wh_id:wh_id).where(status:1).all
+          rescue Exception => e
+            @@logger.error("#{self}.func_get_one_rm(#{wh_id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterialHistory.func_get_all_by_wh_id_and_type(wh_id,type)
+          begin
+            DAO_WHRawMaterialHistory.dataset.where(wh_id:wh_id).where(record_type:type).where(status:1).all
+          rescue Exception => e
+            @@logger.error("#{self}.func_get_one_rm(#{wh_id}) Exception:#{e}")
+            nil
+          end
+        end
+
+        def DAO_WHRawMaterialHistory.func_get_all
+          begin
+            DAO_WHRawMaterialHistory.dataset.where(status:1).all
+          rescue Exception => e
+            @@logger.error("#{self}.func_get_all Exception:#{e}")
+            nil
+          end
+        end
+      end
+    end
+  end
+end
